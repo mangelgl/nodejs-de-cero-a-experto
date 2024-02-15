0ff6093c2352ee32125cd69553ac50983f50bef5
Miguel Ángel García <magarcial.dev@outlook.es>
2024-02-15 14:55:22 +0100

    Aplicación de Monitoreo NOC

---
+++ 05-NOC/src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,34 @@
+
+interface CheckServiceUseCase {
+    execute( url: string ): Promise<boolean>;
+}
+
+type SuccessCallback = () => void;
+type ErrorCallback = ( error: string ) => void;
+
+export class CheckService implements CheckServiceUseCase {
+
+    constructor(
+        /**
+         * Inyección de dependencias
+         */
+        private readonly successCallback: SuccessCallback,
+        private readonly errorCallback: ErrorCallback
+    ){}
+
+    public async execute( url: string ): Promise<boolean> {
+
+        try {
+            const request = await fetch( url );
+            if (!request.ok) throw new Error(`Error on check service ${url}`);
+
+            this.successCallback();            
+            return true;
+        } catch (error) {
+
+            this.errorCallback(`${error}`);
+            return false;
+        }
+
+    }
+}
---
+++ 05-NOC/src/presentation/server.ts
@@ -0,0 +1,22 @@
+import { CronService } from '../cron/cron-service';
+import { CheckService } from '../domain/use-cases/checks/check-service';
+
+export class Server {
+
+    static run () {
+
+        console.log('Server running...');
+
+        const url = 'http://localhost:3000/';
+        CronService.createJob(
+            '*/3 * * * * *',
+            () => {
+                new CheckService(
+                    () => console.log(`${url} is ok`),
+                    ( error ) => console.error( error )
+                ).execute(url);
+            }
+        );
+
+    }
+}
